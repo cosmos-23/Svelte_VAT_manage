ba65114e77a023d2afab0a795f577697a2d94d47
cosmos-23 <cosmos1012@tutanota.com>
2023-11-30 18:16:47 +0900

    open-props global style applied

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -9,3 +9,17 @@
     <p>{description}</p>
     <p>${price}</p>
 </div>
+
+<style>
+    .card {
+        border: 1px solid tomato;
+        width: 500px;
+    }
+    h2 {
+        font-size: 30px;
+        font-weight: var(--font-weight-8);
+    }
+    p {
+        font-weight: var(--font-weight-4);
+    }
+</style>
